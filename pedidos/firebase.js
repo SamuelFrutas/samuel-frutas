83935b06f979f977ceef6724ee9d52d29d15b8f9
SamuelFrutas <samuelmarvila17@gmail.com>
2026-09-02 09:06:08 -0300

    feat(pedidos): conectar Firestore e Authentication do sistema de pedidos

--- pedidos/firebase.js
+++ pedidos/firebase.js
@@ -1,7 +1,9 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js';
 import { PEDIDOS_CONFIG } from './config.js';
 
-const app = initializeApp(PEDIDOS_CONFIG.firebase);
+const app = initializeApp(PEDIDOS_CONFIG.firebase, 'samuel-frutas-pedidos');
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 export { app };
